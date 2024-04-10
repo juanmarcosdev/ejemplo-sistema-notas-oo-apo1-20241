6ffacc8892491c66b0ed4c3e66899776d726c30d
Juan Marcos Caicedo Mejia <1144110058@u.icesi.edu.co>
2024-04-10 12:34:54 -0500

    Arreglando errores

--- src/ui/Executable.java
+++ src/ui/Executable.java
@@ -5,10 +5,10 @@ import model.Controladora;
 
 public class Executable {
 
-	// Variables globales
+	// Atributos de la clase Ejecutable
 	private Scanner reader;
 	private Controladora cont;
-	private boolean flag;
+	private static boolean flag;
 
 	private Executable() {
 		reader = new Scanner(System.in);
@@ -17,7 +17,7 @@ public class Executable {
 	
 	
 
-	public static void run(boolean flag)
+	public void run(boolean flag)
 	{
 
 		flag = false;
@@ -25,7 +25,7 @@ public class Executable {
 		while (!flag) {
 
 			System.out.println("\n \n Bienvenido al menu:\n");
-			System.out.println("Opciones:\n" + "1. Primera opcion \n" + "2. Segunda opcion\n"
+			System.out.println("Opciones:\n" + "1. Registrar profesor \n" + "2. Segunda opcion\n"
 					+ "3. Tercera opcion \n" + "4. Salir del programa");
 
 			int option = reader.nextInt();
@@ -34,13 +34,13 @@ public class Executable {
 
 			switch (option) {
 					case 1:
-						primeraOpcion();
+						registrarProfesor();
 						break;
 					case 2:
-						segundaOpcion();
+						//segundaOpcion();
 						break;
 					case 3:
-						terceraOpcion();
+						//terceraOpcion();
 						break;
 					case 4:
 						flag = true;
@@ -59,6 +59,35 @@ public class Executable {
 		Executable mainApp = new Executable();
 		mainApp.run(flag);
 	}
+	
+	public void registrarProfesor() {
+		
+		String nombreProfe, cedulaProfe;
+		int edadProfe;
+		
+		System.out.println("Profesor, ingrese su nombre");
+		
+		nombreProfe = reader.nextLine();
+		
+		System.out.println("Profesor, ingrese su edad");
+		
+		edadProfe = reader.nextInt();
+		
+		reader.nextLine(); // Limpiar el buffer
+		
+		System.out.println("Profesor, ingrese su cedula");
+		
+		cedulaProfe = reader.nextLine();
+		
+		System.out.println("Datos del profe: \n" + 
+		"Nombre: " + nombreProfe + "\n" + 
+		"Edad: " + edadProfe + "\n" + 
+		"Cedula: " + cedulaProfe);
+		
+		
+		//cont.crearProfesor(
+	}
+
 
 
 	
